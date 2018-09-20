819b61f3b4373ee3ac6517eea1c603f943d99f01
Joe Krause <jkrause@student.42.us.org>
2018-09-20 15:40:42 -0700

    Commit debug version, fully working according to tester.

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,18 +6,37 @@
 /*   By: jkrause <jkrause@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/14 23:29:20 by jkrause           #+#    #+#             */
-/*   Updated: 2018/08/14 23:50:53 by jkrause          ###   ########.fr       */
+/*   Updated: 2018/09/08 13:31:04 by jkrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rbtree.h"
 
-
 /*
-** There's a O(log n) reason for this file to be called algorithm.
+** Used for comparison only.
 */
 
-void				rb_insert_i(t_node *root)
+void			rb_fix_insert_recurs(t_node *q)
 {
-	
+	t_node		*p;
+	t_node		*g;
+
+	if (!q)
+		return ;
+	p = q->parent;
+	ft_printf("q: %d p: %d\n", q->value, (p ? p->value : 0));
+	g = (p ? p->parent : 0);
+	if (CBOTHRED(q) && q->red == 0)
+	{
+		rb_invert(q);
+	}
+	if (g && p && BOTHRED(q, p))
+	{
+		if (GETCHILD(q) != GETCHILD(p))
+		{
+			p = rb_rotate(p, !GETCHILD(q), 0);
+		}
+		return (rb_fix_insert_recurs(rb_rotate(g, !GETCHILD(p), 1)));
+	}
+	return (rb_fix_insert_recurs(q->parent));
 }
--- src/assert.c
+++ src/assert.c
@@ -6,7 +6,7 @@
 /*   By: jkrause <jkrause@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 13:12:36 by jkrause           #+#    #+#             */
-/*   Updated: 2018/09/08 11:03:47 by jkrause          ###   ########.fr       */
+/*   Updated: 2018/09/13 15:04:37 by jkrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void			fail(char *str)
 
 void			fail2(char *str, int a, int b)
 {
+	print_buds(g_root, -1);
 	FAIL2(str, a, b);
 }
 
@@ -46,49 +47,54 @@ int				rb_assert(t_node *n)
 	int			lh;
 	int			rh;
 
-	ft_printf("AssertNode: %d is Red? %d\n", (n == 0 ? -1 : n->value), (n == 0 ? -1 : n->red));
+	//ft_printf("AssertNode: %d is Red? %d\n", (n == 0 ? -1 : n->value), (n == 0 ? -1 : n->red));
 	if (!n)
 		return (1);
 	if (n->children[0] == 0 && n->children[1] == 0)
 	{
-		ft_printf("No children(%d): return appropriate value\n", n->value);
+		//ft_printf("No children(%d): return appropriate value\n", n->value);
 		return (n->red == 1 ? 0 : 1);
 	}
 	if ((n->children[0] && n->children[0]->parent != n)
 			|| (n->children[1] && n->children[1]->parent != n))
 	{
 		ft_printf("**** rb assert: Found dangling parent on node %d (p: %d).\n", n->value, (n->parent ? n->parent->value : -1));
+		print_buds(g_root, -1);
 		abort();
 	}
 	if (n->red == 0 && (n->children[0] != 0 && n->children[0]->red == 0) &&
 			(n->children[1] != 0 && n->children[1] == 0))
 	{
 		ft_printf("**** rb assert: Got a B violation (p -> l and r children) (at %d p: %d)\n", n->value, (n->parent ? n->parent->value : -1));
+		print_buds(g_root, -1);
 		abort();
 	}
 	if (IS_RED(n) == 1 && (IS_RED(n->children[0]) || IS_RED(n->children[1])))
 	{
 		ft_printf("**** rb assert: Red violation (RedParent->RedChild) (node: %d, p: %d)\n", n->value, (n->parent ? n->parent->value : -1));
+		print_buds(g_root, -1);
 		abort();
 	}
 	if (n->children[LEFT] != 0 && n->children[0]->value > n->value)
 	{
 		ft_printf("**** rb assert: B-tree violation (LChild->Val > Node->Value) (%d > %d)", n->children[0]->value, n->value);
+		print_buds(g_root, -1);
 		abort();
 	}
 	if (n->children[RIGHT] != 0 && n->children[1]->value < n->value)
 	{
 		ft_printf("B-tree violation (RChild->Val < Node->Value) (%d < %d)", n->children[1]->value, n->value);
+		print_buds(g_root, -1);
 		abort();
 	}
 	lh = rb_assert(n->children[LEFT]);
 	rh = rb_assert(n->children[RIGHT]);
-	ft_printf("%d %d\n", lh, rh);
+	//ft_printf("%d %d\n", lh, rh);
 	if (lh != 0 && rh != 0 && lh != rh)
 	{
 		fail2("Black-height violation (LH != RH)", lh, rh);
 	}
-	ft_printf("Return value(%d): %d\n", n->value, (n->red == 1 ? lh : lh + 1));
+	//ft_printf("Return value(%d): %d\n", n->value, (n->red == 1 ? lh : lh + 1));
 	if (lh == 0 || rh == 0)
 		return (0);
 	return (n->red == 1 ? lh : lh + 1);
